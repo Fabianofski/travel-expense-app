64c088e67dcff06dea78aa8568b4ae50dfd492b1
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-05-25 21:14:06 +0200

    Added some placeholder content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 </script>
 
-<nav class="bg-black p-4">
-	<ul class="flex gap-2 text-lg text-white">
-		<li><a href="/home">Home</a></li>
-		<li><a href="/about">About</a></li>
-		<li><a href="/expenses">Expenses</a></li>
-	</ul>
-</nav>
-<slot />
+<div class="bg-black min-h-screen">
+	<nav
+		class="bg-violet-600 p-4 backdrop-blur bg-opacity-10 fixed w-full z-10 border-b border-violet-600"
+	>
+		<ul class="flex gap-2 text-lg text-white">
+			<li><a href="/home">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/expenses">Expenses</a></li>
+		</ul>
+	</nav>
+	<div class="pt-16 w-full h-full">
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<p>Travel Expense App</p>
+<script lang="ts">
+</script>
+
+<h1
+	class="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-bl from-purple-400 via-zinc-300 to-fuchsia-600"
+>
+	TRAVEL EXPENSE APP
+</h1>
