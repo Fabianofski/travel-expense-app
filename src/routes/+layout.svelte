675be9f15a0db9dcd832661cb6f3645be2126060
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-05-25 19:27:49 +0200

    Added Svelte project with tailwind css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="bg-black p-4">
+	<ul class="flex gap-2 text-lg text-white">
+		<li><a href="/home">Home</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/expenses">Expenses</a></li>
+	</ul>
+</nav>
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<p>Travel Expense App</p>
