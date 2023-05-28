59025177cc9980cc7c5d3605bde5aa036e49a62e
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-05-28 19:37:15 +0200

    Added password authentication to list

--- src/routes/api/expenseList/+server.ts
+++ src/routes/api/expenseList/+server.ts
@@ -1,16 +1,21 @@
-import { error, json } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
 import { database } from '../../../lib/client/firebase';
+import type { ExpenseListModel } from '../../../models/expenseListModel';
 
 export async function GET({ url }) {
 	console.log(url);
 	const id = url.searchParams.get('expenseListId');
+	const password = url.searchParams.get('password');
 	const ref = database.ref('expenses/' + id);
 	return ref
 		.get()
 		.then((snapshot) => {
-			return json(snapshot.val());
+			const data: ExpenseListModel = snapshot.val();
+			if (!data) return new Response(null, { status: 404, statusText: 'List not found!' });
+			else if (data.password === password) return json(snapshot.val());
+			else return new Response(null, { status: 403, statusText: 'Permission denied!' });
 		})
 		.catch(() => {
-			return json('fail');
+			return new Response(null, { status: 500, statusText: 'Internal Server Error!' });
 		});
 }
--- src/routes/list/[id]/+page.svelte
+++ src/routes/list/[id]/+page.svelte
@@ -1,16 +1,74 @@
 <script lang="ts">
+	import { error } from '@sveltejs/kit';
 	import ExpenseList from './expenseList.svelte';
 	import PersonExpenseList from './personExpenseList.svelte';
 	import type { ExpenseListModel } from '../../../models/expenseListModel';
+	import { expensesStore } from './store';
 
 	export let data;
-	const expenseListData: ExpenseListModel = data.expenseListData;
+	let loading = false;
+	let unlocked = false;
+	let loginError = '';
+	let password = '';
+	let participants: string[];
+
+	async function unlockList() {
+		loginError = '';
+		loading = true;
+
+		const response = await fetch(
+			`/api/expenseList?` +
+				new URLSearchParams({
+					expenseListId: data.id,
+					password: password
+				})
+		);
+
+		if (response.status === 403) {
+			loading = false;
+			loginError = 'Permission denied.';
+			return;
+		} else if (response.status === 404) throw error(404, 'List not found!');
+		else if (response.status !== 200) throw Error('Internal Error');
+
+		const list: ExpenseListModel = await response.json();
+		if (!list.expenses) list.expenses = [];
+
+		participants = list.participants;
+		expensesStore.set(list.expenses);
+		loading = false;
+		unlocked = true;
+	}
 </script>
 
 <div class="flex flex-col gap-16 pt-4">
-	<ExpenseList expenses={expenseListData.expenses} participants={expenseListData.participants} />
-	<PersonExpenseList
-		expenses={expenseListData.expenses}
-		participants={expenseListData.participants}
-	/>
+	{#if unlocked}
+		<ExpenseList {participants} />
+		<PersonExpenseList {participants} />
+	{:else}
+		<div class="flex flex-col gap-2">
+			<form class="w-full flex flex-col items-center">
+				<div class="form-control w-full max-w-s">
+					<label class="label" for="">
+						<span class="label-text">Passwort</span>
+					</label>
+					<input
+						bind:value={password}
+						type="password"
+						placeholder="Passwort..."
+						class="input input-bordered w-full max-w-s"
+						required
+					/>
+				</div>
+			</form>
+			<button class="btn btn-primary" on:click={unlockList} disabled={loading}>
+				{#if loading}
+					<img class="h-2" src="/loading.svg" alt="loading" />
+				{:else}
+					Entsperren
+				{/if}
+			</button>
+			<p>{loginError}</p>
+		</div>
+	{/if}
 </div>
