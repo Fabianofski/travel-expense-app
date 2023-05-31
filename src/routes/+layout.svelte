916c6b120ba830ba643cf83735a407d1b3884c69
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-05-31 21:30:48 +0200

    Changed Navbar color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <div class="bg-black min-h-screen flex flex-col items-center">
 	<NavBar />
-	<div class="px-2 pt-16 pb-4 w-full h-full max-w-3xl">
+	<div class="px-2 pt-16 mt-2 pb-4 w-full h-full max-w-3xl">
 		<slot />
 	</div>
 </div>
