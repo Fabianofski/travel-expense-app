83a31a6d3c83473200f0fb72ed2f381d6ca33323
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-06-06 21:27:27 +0200

    Removed preserved keyword FIREBASE from .env

--- src/lib/client/firebase.ts
+++ src/lib/client/firebase.ts
@@ -1,18 +1,13 @@
 import admin from 'firebase-admin';
-import {
-	FIREBASE_PRIVATE_KEY,
-	FIREBASE_PROJECT_ID,
-	FIREBASE_CLIENT_EMAIL,
-	FIREBASE_DATABASE_URL
-} from '$env/static/private';
+import { PRIVATE_KEY, PROJECT_ID, CLIENT_EMAIL, DATABASE_URL } from '$env/static/private';
 
 const firebaseConfig = {
 	credential: admin.credential.cert({
-		projectId: FIREBASE_PROJECT_ID,
-		privateKey: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-		clientEmail: FIREBASE_CLIENT_EMAIL
+		projectId: PROJECT_ID,
+		privateKey: PRIVATE_KEY?.replace(/\\n/g, '\n'),
+		clientEmail: CLIENT_EMAIL
 	}),
-	databaseURL: FIREBASE_DATABASE_URL
+	databaseURL: DATABASE_URL
 };
 
 admin.initializeApp(firebaseConfig);
