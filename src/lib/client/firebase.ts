d60aea21204c8ac6c7018fdd2b52834f136e6b1a
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-06-06 19:27:58 +0200

    Moved firebase secrets to .env

--- src/lib/client/firebase.ts
+++ src/lib/client/firebase.ts
@@ -1,9 +1,18 @@
 import admin from 'firebase-admin';
-import { credential, databaseURL } from './credentials';
+import {
+	FIREBASE_PRIVATE_KEY,
+	FIREBASE_PROJECT_ID,
+	FIREBASE_CLIENT_EMAIL,
+	FIREBASE_DATABASE_URL
+} from '$env/static/private';
 
 const firebaseConfig = {
-	credential: admin.credential.cert(credential),
-	databaseURL: databaseURL
+	credential: admin.credential.cert({
+		projectId: FIREBASE_PROJECT_ID,
+		privateKey: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+		clientEmail: FIREBASE_CLIENT_EMAIL
+	}),
+	databaseURL: FIREBASE_DATABASE_URL
 };
 
 admin.initializeApp(firebaseConfig);
